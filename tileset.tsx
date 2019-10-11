cebb34f6f13dfd13c2bcf407c46360f44eda3c1a
librolibgdx1 <56313976+librolibgdx1@users.noreply.github.com>
2019-10-11 19:29:54 +0200

    Add files via upload

---
+++ tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2019.10.08" name="tileset" tilewidth="16" tileheight="16" spacing="2" margin="2" tilecount="784" columns="28">
+ <image source="../../../tileset.png" width="512" height="512"/>
+</tileset>
